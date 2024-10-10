6a30947ac5dfc3f58a7a63ca1e3863c5b30cf17d
hany0ageeb <hany0ageeb@gmail.com>
2024-10-10 11:56:03 +0200

    Task 1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function concat that takes a string str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    return the concatenated string from str1, str2
+    Args:
+        str1: string
+        str2: string
+    Return
+        str1 + str2
+    """
+    return str1 + str2
