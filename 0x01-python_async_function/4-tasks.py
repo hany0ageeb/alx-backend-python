a222dcf7e12712627d7f8d125fdbfcaf6f20fe65
hany0ageeb <hany0ageeb@gmail.com>
2024-10-14 11:14:39 +0200

    Task 4. Tasks - project 0x01-python_async_function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# 4-tasks.py
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
+"""
+from typing import Callable, List
+import asyncio
+
+task_wait_random: Callable[[int], asyncio.Task] = __import__(
+    '3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    code is nearly identical to wait_n except task_wait_random is being called
+    """
+    tasks: List[asyncio.Task] = [task_wait_random(max_delay) for i in range(n)]
+    l: List[float] = await asyncio.gather(*tasks)
+    l = sorted(l)
+    return l
