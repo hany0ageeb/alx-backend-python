3030c4212d2df2e8e211d6b38961ed5371e18115
hany0ageeb <hany0ageeb@gmail.com>
2024-10-14 08:23:17 +0000

    Fixing pycodestyle issues with 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,7 @@
 """
 Import wait_random from the previous python file that you’ve written and
 write an async routine called wait_n that takes in 2 int arguments
-(in this order): 
+(in this order):
 n and max_delay.
 You will spawn wait_random n times with the specified max_delay
 wait_n should return the list of all the delays (float values).
@@ -29,5 +29,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         The list of the delays should be in ascending order
         without using sort() because of concurrency.
     """
-    l = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
-    return sorted(l)
+    result: List[float] =
+    await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    result = sorted(result)
+    return result
