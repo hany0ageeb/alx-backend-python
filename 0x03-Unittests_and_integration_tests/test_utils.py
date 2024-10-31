6e304f4c0ddfb1638bac1f60d3f678040d2d039b
hany0ageeb <hany0ageeb@gmail.com>
2024-10-31 20:52:27 +0300

    task 0.Parameterize a unit test + task 1.Parameterize a unit test + task 2. Mock HTTP calls

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+"""
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap
+    """
+    @parameterized.expand([
+        ({"a": 1}, ('a',), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 3}}, ("a", "b"), 3),
+        ({"a": {"b": {"c": 0}}}, ('a', 'b', 'c'), 0),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """test_access_nested_map
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson
+    """
+    @parameterized.expand([
+        ('http://example.com', {"payload": True}),
+        ('http://holberton.io', {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        with patch('utils.requests.get') as mock:
+            mock_response = Mock()
+            mock_response.json = Mock(return_value=test_payload)
+            mock.return_value = mock_response
+            result = get_json(test_url)
+            mock.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize
+    """
+
+    def test_memoize(self):
+        """test_memoize
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
