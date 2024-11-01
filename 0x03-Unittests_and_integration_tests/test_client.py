6c135a2f1049c777ae4afe58cbba330a410ea36d
hany0ageeb <hany0ageeb@gmail.com>
2024-11-01 21:10:15 +0200

    Task 7. Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -49,3 +49,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, ["truth", "ruby-openid-apps-discovery"])
             m.assert_called_once()
             mocked_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """test_has_license
+        """
+        self.assertEqual(GithubOrgClient.has_license(
+            repo, license_key), expected)
